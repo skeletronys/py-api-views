9da2b2d9552a0b418cafd51c190306dc45e9ad4a
skeletronys <mykytcey@gmail.com>
2024-05-28 23:12:30 +0200

    fixed test, delete in MovieSerializer (ModelSerialize)

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from cinema.models import Movie, CinemaHall, Actor, Genre
 
 
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = "__all__"
-
-
 class CinemaHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = CinemaHall
@@ -25,3 +19,34 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = "__all__"
+
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    actors = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all(), many=True
+    )
+    genres = serializers.PrimaryKeyRelatedField(
+        queryset=Genre.objects.all(), many=True
+    )
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
+
+    def create(self, validated_data):
+        actors = validated_data.pop("actors")
+        genres = validated_data.pop("genres")
+        movie = Movie.objects.create(**validated_data)
+        movie.actors.set(actors)
+        movie.genres.set(genres)
+        return movie
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.duration = validated_data.get("duration", instance.duration)
+        instance.description = validated_data.get(
+            "description",
+            instance.description
+        )
+        instance.save()
+        return instance
